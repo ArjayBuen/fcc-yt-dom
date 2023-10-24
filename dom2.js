6505b6aa04b1d0f42a083e6768afafbed24eb0b0
ArjayBuen <arjaybuen10@gmail.com>
2023-10-24 21:21:12 +0800

    learning event propogation

--- dom2.js
+++ dom2.js
@@ -34,3 +34,25 @@ function revealContent(){
 }
 
 revealBtn.addEventListener('click',revealContent);
+
+
+//Event Propogation
+
+window.addEventListener("click", function(){
+    console.log('Window');
+}, true);
+document.addEventListener("click", function(){
+    console.log('Dcoument')
+}, true)
+document.querySelector('.div2').addEventListener("click", function(){
+    // e.stopPropagation();
+    console.log('DIV 2');
+},{once: true} );
+
+document.querySelector('.div1').addEventListener("click", function(){
+    console.log('DIV 1');
+}, true);
+
+document.querySelector('.btn').addEventListener("click", function(e){
+    console.log(e.target.innerText='clicked');
+}, true);
