e4fdf8300a8bde07d704a26f52072c519ffa37a1
ArjayBuen <arjaybuen10@gmail.com>
2023-10-29 22:26:21 +0800

    learning event delegation

--- dom2.js
+++ dom2.js
@@ -45,14 +45,65 @@ document.addEventListener("click", function(){
     console.log('Dcoument')
 }, true)
 document.querySelector('.div2').addEventListener("click", function(){
-    // e.stopPropagation();
+    //e.stopPropagation();
     console.log('DIV 2');
-},{once: true} );
+},{once: true}/*true*/ );
 
 document.querySelector('.div1').addEventListener("click", function(){
     console.log('DIV 1');
 }, true);
 
 document.querySelector('.btn').addEventListener("click", function(e){
+    e.preventDefault();
     console.log(e.target.innerText='clicked');
 }, true);
+
+
+//Event Delegation
+// document.querySelector('.Football').addEventListener("click",function(e){
+//     console.log('football is clicked');
+//     const target=e.target;
+//     if(target.matches('li')){
+//         target.style.backgroundColor="lightgray";
+//     }
+// })
+
+// document.querySelector('.basketball').addEventListener("click",function(e){
+//     console.log('basketball is clicked');
+//     const target=e.target;
+//     if(target.matches('li')){
+//         target.style.backgroundColor="lightgray";
+//     }
+// })
+
+// document.querySelector('.boxing').addEventListener("click",function(e){
+//     console.log('boxing is clicked');
+//     const target=e.target;
+//     if(target.matches('li')){
+//         target.style.backgroundColor="lightgray";
+//     }
+// })
+
+// document.querySelector('.tennis').addEventListener("click",function(e){
+//     console.log('tennis is clicked');
+//     const target=e.target;
+//     if(target.matches('li')){
+//         target.style.backgroundColor="lightgray";
+//     }
+// })
+
+// document.querySelector('.golf').addEventListener("click",function(e){
+//     console.log('golf is clicked');
+//     const target=e.target;
+//     if(target.matches('li')){
+//         target.style.backgroundColor="lightgray";
+//     }
+// })
+
+document.querySelector('#sports-list').addEventListener("click",function(e){
+    console.log(e.target.getAttribute('id')+ ' is clicked');
+    const target= e.target;
+    if(target.matches('li')){
+                 target.style.backgroundColor="lightgray";
+             }
+})
